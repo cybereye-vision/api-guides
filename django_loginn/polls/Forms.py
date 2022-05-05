bcf7aea88c3720202f32d798558ed5b7dcbb135e
duongvanthanh2011 <58882839+duongvanthanh2011@users.noreply.github.com>
2022-05-05 15:27:48 +0700

    Delete django_loginn directory

--- django_loginn/polls/Forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class register (forms.Form):
-    username = forms.CharField(label="Tài Khoản",max_length=30)
-    pasword = forms.CharField(label="Mật khẩu", widget=forms.PasswordInput())
